d5f47c565445b2212e8f6f98a154b196579ff862
zhouhongliang <122551267@qq.com>
2019-02-15 10:00:11 +0800

    用户登录功能实现完毕

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,5 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+    <div>后台首页</div>
 </template>
 
 <script>
@@ -9,5 +7,5 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
 </style>
---
+++ src/components/Login.vue
@@ -0,0 +1,106 @@
+<template>
+  <div id="login-container">
+    <div id="login-box">
+      <div id="logo-box">
+        <img src="../assets/img/logo.png">
+      </div>
+      <el-form :rules="loginFormRules" ref="loginFormRef" :model="loginForm">
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username">
+            <i slot="prefix" class="iconfont icon-user"></i>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input type="password" v-model="loginForm.password">
+            <i slot="prefix" class="iconfont icon-3702mima"></i>
+          </el-input>
+        </el-form-item>
+        <el-row>
+          <el-col :offset="15">
+            <el-button type="primary" @click="login">登录</el-button>
+            <el-button type="info" @click="reset">重置</el-button>
+          </el-col>
+        </el-row>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      loginFormRules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' }
+        ],
+        password: [{ required: true, message: '请输入密码', trigger: 'blur' }]
+      }
+    }
+  },
+  methods: {
+    login() {
+      this.$refs.loginFormRef.validate(async valid => {
+        if (valid) {
+          const {data: res} = await this.$http.post('/login', this.loginForm)
+          if (res.meta.status !== 200) {
+            return this.$message.error('用户名或密码错误')
+          }
+          window.sessionStorage.setItem('token', res.data.token)
+          this.$router.push('/home')
+        }
+      })
+    },
+    reset() {
+      this.$refs.loginFormRef.resetFields()
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+#login-container {
+  height: 100%;
+  background-color: #2b4b6b;
+  overflow: hidden;
+  #login-box {
+    width: 450px;
+    height: 304px;
+    background-color: #fff;
+    border-radius: 4px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    #logo-box {
+      width: 130px;
+      height: 130px;
+      border: 1px solid #eee;
+      border-radius: 50%;
+      padding: 8px;
+      box-shadow: 0 0 10px #eee;
+      position: absolute;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background-color: #fff;
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        background-color: #eee;
+      }
+    }
+    .el-form {
+      position: absolute;
+      bottom: 0;
+      padding: 20px;
+      width: 100%;
+      box-sizing: border-box;
+    }
+  }
+}
+</style>
